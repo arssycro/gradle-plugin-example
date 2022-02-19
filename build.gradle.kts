dff9c56ab17c7b1d6e65aeb789c767903e83d1b6
Josh Smith <josh.smith84@gmail.com>
2022-02-18 23:01:52 -0600

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import Build_gradle.UberExtension.Companion.uber
+
 plugins {
     kotlin("jvm") version "1.5.10"
     `kotlin-dsl`
@@ -11,41 +13,60 @@ dependencies {
     implementation(kotlin("stdlib"))
 }
 
+interface UberOperation {
+    fun apply(project: Project)
+}
+
+class JunitOperation : UberOperation {
+    override fun apply(project: Project) {
+        project.tasks.getByName<Test>("test").apply {
+            useJUnitPlatform()
+        }
+
+        val testDependencies = project.configurations.getByName("testImplementation").dependencies
+        project.gradle.addListener(object : DependencyResolutionListener {
+            override fun beforeResolve(dependencies: ResolvableDependencies) {
+                testDependencies.add(project.dependencies.create("org.junit.jupiter:junit-jupiter-api:5.8.1"))
+                testDependencies.add(project.dependencies.create("org.junit.jupiter:junit-jupiter-engine:5.8.1"))
+                project.gradle.removeListener(this)
+            }
+
+            override fun afterResolve(dependencies: ResolvableDependencies) {}
+        })
+    }
+}
+
 open class UberExtension {
-    internal var junit: Boolean = false
+    internal val operations: MutableList<UberOperation> = mutableListOf()
 
     fun junit() {
-        junit = true
+        operations.add(JunitOperation())
+    }
+
+    companion object {
+        fun Project.uber(): UberExtension {
+            return extensions.create("uber", UberExtension::class.java)
+        }
+
+        fun Project.uber(block: UberExtension.() -> Unit): UberExtension {
+            return extensions.getByName<UberExtension>("uber").apply(block)
+        }
     }
 }
 
 class UberPlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = project.run {
-        val extension = extensions.create("uber", UberExtension::class.java)
+        val extension = uber()
 
         afterEvaluate {
-            if (extension.junit) {
-                tasks.getByName<Test>("test").apply {
-                    useJUnitPlatform()
-                }
-
-                val testDependencies = configurations.getByName("testImplementation").dependencies
-                gradle.addListener(object : DependencyResolutionListener {
-                    override fun beforeResolve(dependencies: ResolvableDependencies) {
-                        testDependencies.add(project.dependencies.create("org.junit.jupiter:junit-jupiter-api:5.8.1"))
-                        testDependencies.add(project.dependencies.create("org.junit.jupiter:junit-jupiter-engine:5.8.1"))
-                        gradle.removeListener(this)
-                    }
-
-                    override fun afterResolve(dependencies: ResolvableDependencies) {}
-                })
-            }
+            extension.operations.forEach { it.apply(project) }
         }
     }
 }
 
 apply<UberPlugin>()
 
-configure<UberExtension> {
+uber {
+    // If you uncomment the following line, the example test class will no longer compile.
     junit()
 }
