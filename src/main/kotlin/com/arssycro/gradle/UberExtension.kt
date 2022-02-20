87c7529dcd32348c1b2f89f82e0e994423ed6230
Josh Smith <josh.smith84@gmail.com>
2022-02-20 01:24:17 -0600

    Cleanup

--- src/main/kotlin/com/arssycro/gradle/UberExtension.kt
+++ src/main/kotlin/com/arssycro/gradle/UberExtension.kt
@@ -3,8 +3,6 @@ package com.arssycro.gradle
 import com.arssycro.gradle.operation.JunitOperation
 import com.arssycro.gradle.operation.SpringOperation
 import com.arssycro.gradle.operation.UberOperation
-import org.gradle.api.Action
-import org.gradle.api.Project
 
 open class UberExtension {
     internal val operations: MutableList<UberOperation> = mutableListOf()
@@ -15,11 +13,5 @@ open class UberExtension {
 
     fun spring() {
         operations.add(SpringOperation())
-    }
-
-    companion object {
-        fun Project.uber(action: Action<UberExtension>) {
-            extensions.configure("uber", action)
-        }
     }
 }
